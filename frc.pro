e0abc4c3ded0b51227fec391987fa52cb77b38ce
zhangtalent <1322293658@qq.com>
2023-09-30 20:09:59 +0800

    fff

--- frc.pro
+++ frc.pro
@@ -14,9 +14,9 @@ TEMPLATE = app
 # any feature of Qt which has been marked as deprecated (the exact warnings
 # depend on your compiler). Please consult the documentation of the
 # deprecated API in order to know how to port your code away from it.
-#DEFINES += QT_DEPRECATED_WARNINGS
+DEFINES += QT_DEPRECATED_WARNINGS
 
-DEFINES+=QT_NO_DEBUG_OUTPUT
+#DEFINES+=QT_NO_DEBUG_OUTPUT
 
 # You can also make your code fail to compile if you use deprecated APIs.
 # In order to do so, uncomment the following line.
@@ -24,6 +24,7 @@ DEFINES+=QT_NO_DEBUG_OUTPUT
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 CONFIG += c++11
+CONFIG += static
 
 SOURCES += \
         main.cpp
--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ public:
         connect(button, &QPushButton::clicked, this, &FrcApp::get_exam_info);
 
         QString connect_to_server = connect_udp();
-        qDebug() << "error:" << connect_to_server;
+        //qDebug() << "error:" << connect_to_server;
         if (connect_to_server != "200")
         {
             QMessageBox message_box(this);
@@ -102,7 +102,7 @@ private slots:
     {
         // 接受客户端连接
         QTcpSocket *clientSocket = server.nextPendingConnection();
-        qDebug() << "Client connected:" << clientSocket->peerAddress().toString() << clientSocket->peerPort();
+        //qDebug() << "Client connected:" << clientSocket->peerAddress().toString() << clientSocket->peerPort();
 
         // 处理数据接收
         connect(clientSocket, &QTcpSocket::readyRead, this, &FrcApp::handleReadyRead);
@@ -111,41 +111,103 @@ private slots:
 
     void handleReadyRead()
     {
-        QTcpSocket *clientSocket = qobject_cast<QTcpSocket*>(sender());
-        if (!clientSocket)
+        QObject *senderObj = sender();
+        if (!senderObj)
             return;
+        QTcpSocket *clientSocket = qobject_cast<QTcpSocket*>(sender());
+        if (!clientSocket){
+            return;
+        }
 
-        QByteArray receivedData = clientSocket->readAll();
-        qDebug() << "Received data:" << receivedData;
 
-        QString filePath = receivedData;
-        filePath = filePath.split('|').at(0);
-
-        // 提取文件名
-        QString filename = QDir::homePath() + "/" + QRegularExpression("[^\\\\/:*?\"<>|\\r\\n]+$").match(filePath).captured();
-
-        // 接收文件数据并写入文件
-        QFile file(filename);
-        if (file.open(QIODevice::WriteOnly))
+        while (clientSocket->bytesAvailable() > 0)
         {
-            while (clientSocket->bytesAvailable() > 0)
+
+            QByteArray chunk = clientSocket->read(1024);
+            newData.append(chunk);
+            //qDebug() << "Received data:" << newData;
+            if (receivingFileName)
             {
-                QByteArray data = clientSocket->readAll();
-                file.write(data);
+                if(newData.contains("\r\n")){
+
+                    int endIndex = newData.indexOf("\r\n");
+                    QString recData = newData.left(endIndex);
+                    QString filePath = recData.split('|').at(0);
+                    file_size_and_crc_32 = recData.split("|").at(1);
+                    // 提取文件名
+                    filename = QDir::homePath() + "/" + QRegularExpression("[^\\\\/:*?\"<>|\\r\\n]+$").match(filePath).captured();
+    //                qDebug() << filename;
+                   qDebug() << "Received tes:" << newData;
+                    receivingFileName = false;
+                    receivingFileData = true;
+                    newData.clear();
+                    // 打开文件
+                    file.setFileName(filename);
+                    if (!file.open(QIODevice::WriteOnly))
+                    {
+                        qDebug() << "Failed to open file for writing:" << filename;
+                        return;
+                    }
+                }
+
+                //break;
+            } else if (receivingFileData){
+
+                bool ok;
+                quint32 file_size_receive = file_size_and_crc_32.split("*").at(0).toUInt(&ok);
+                if (!ok) {
+//                    qDebug() << "convert_fail:";
+                    // 转换失败的处理逻辑
+                }
+                if(!newData.isEmpty()){
+                    file.write(newData);
+                }
+
+                receivedSize += newData.size();
+                qDebug() << "size" << receivedSize;
+                newData.clear();
+                if(receivedSize >= file_size_receive){
+
+                 file.close();
+                 QString crc_32_receive = file_size_and_crc_32.split("*").at(1);
+                 QString crc_32_file = calculate_crc32(filename).toUpper();
+                 qint64 file_size = get_file_size(filename);
+                 qint64 fileSize = file_size_and_crc_32.split("*").at(0).toUInt();
+//                 qDebug() << crc_32_receive << crc_32_file << file_size << fileSize;
+                 if(file_size != fileSize || crc_32_receive != crc_32_file){
+                     //qDebug() << "File 111:" << filename;
+                     QMessageBox message_box(this);
+                     message_box.setWindowTitle("提示");
+                     message_box.setText(QString("收到文件，已保存到%1,CRC校验失败【%2】").arg(filename, crc_32_file));
+                     //message_box.exec();
+                 } else{
+                     //qDebug() << "File 222:" << filename;
+                     QMessageBox message_box(this);
+                     message_box.setWindowTitle("提示");
+                     message_box.setText(QString("收到文件，已保存到%1,CRC校验通过【%2】").arg(filename, crc_32_file));
+                     //message_box.exec();
+                 }
+                 //qDebug() << "File received and saved:" << filename;
+                 // 关闭当前客户端连接
+//
+                 receivingFileName = true;
+                 receivingFileData = false;
+                 receivingFileOver = true;
+                 break;
+                } else{
+                        qDebug() << "waiting data:" ;
+                }
+                //qDebug() << "Received data:" ;
+            } else {
             }
-
-            qDebug() << "File received:" << filename;
-        }
-        else
-        {
-            qDebug() << "Failed to open file:" << filename;
         }
 
-        file.close();
+        if(receivingFileOver){
+            clientSocket->close();
+            newData.clear();
+            receivingFileOver = false;
+        }
 
-        // 关闭当前客户端连接
-        clientSocket->close();
-        clientSocket->deleteLater();
     }
 
 
@@ -164,6 +226,7 @@ private:
     QPushButton *button;
     QLabel *local_ip_text;
     QTimer timer;
+    QByteArray newData;
     QString server_ip;
     QString group_ip = "224.0.0.1";
     quint16 port = 20000;
@@ -172,14 +235,22 @@ private:
     QUdpSocket udpSocket;
     // 创建服务器对象
     QTcpServer server;
+    QString filename;
+    QString file_size_and_crc_32;
+    QFile file;
+    bool receivingFileName = true;
+    bool receivingFileData = false;
+    bool receivingFileOver = false;
+    quint64 fileSize = 0;
+    quint64 receivedSize = 0;
 
     void get_exam_info()
     {
         QString exam_number = exam_input->text();
         QString folder_path = folder_input->text();
-        qDebug() << "准考证号:" << exam_number;
-        qDebug() << "工作文件夹:" << folder_path;
-        qDebug() << "IP:" << server_ip;
+//        qDebug() << "准考证号:" << exam_number;
+//        qDebug() << "工作文件夹:" << folder_path;
+//        qDebug() << "IP:" << server_ip;
         QMessageBox message_box(this);
         message_box.setWindowTitle("提示");
         message_box.setText(QString("你的准考证号是: %1\n工作文件夹是: %2\n设置成功！").arg(exam_number, folder_path));
@@ -200,7 +271,7 @@ private:
 
     QString connect_udp()
     {
-         qDebug() << "xxxxxx" << "111";
+//         qDebug() << "xxxxxx" << "111";
         try
         {
             udpSocket.bind(QHostAddress::AnyIPv4, port);
@@ -209,11 +280,11 @@ private:
             foreach (const QNetworkInterface& interface, interfaces) {
                 if (interface.flags() & QNetworkInterface::IsUp) {
                     if (udpSocket.joinMulticastGroup(QHostAddress(group_ip), interface)) {
-                        qDebug() << "Joined multicast group on interface:" << interface.name();
+//                        qDebug() << "Joined multicast group on interface:" << interface.name();
                         result  = "200";
                         break;
                     } else {
-                        qDebug() << "Failed to join multicast group on interface:" << interface.name();
+//                        qDebug() << "Failed to join multicast group on interface:" << interface.name();
                     }
                 }
             }
@@ -221,7 +292,7 @@ private:
         }
         catch (const std::exception &e)
         {
-            qDebug() << "xxxxxx" << e.what();
+//            qDebug() << "xxxxxx" << e.what();
             return QString::fromStdString(e.what());
         }
     }
@@ -237,11 +308,11 @@ private:
         // 启动服务器
         if (!server.listen(QHostAddress::AnyIPv4, 20001))
         {
-            qDebug() << "Failed to start server:" << server.errorString();
+//            qDebug() << "Failed to start server:" << server.errorString();
             return;
         }
 
-        qDebug() << "Server started. Waiting for client connections...";
+//        qDebug() << "Server started. Waiting for client connections...";
     }
 
     void receive_thread()
@@ -265,12 +336,12 @@ private:
                 ip_addr = code_and_ip.mid(1);
             }
 
-            qDebug() << datagram << code << get_local_ip() << sender << ip_addr;
+//            qDebug() << datagram << code << get_local_ip() << sender << ip_addr;
 
             if (code == "D" && ip_addr == get_local_ip())
             {
                 server_ip = sender.toString();
-                qDebug() << server_ip;
+//                qDebug() << server_ip;
             }
             else if (code == "4")
             {
@@ -283,7 +354,7 @@ private:
             }
             else if (code == "2" && ip_addr == get_local_ip())
             {
-                qDebug() << "2";
+//                qDebug() << "2";
                 QByteArray message = QString("3%1").arg(server_ip).toUtf8();
                 udpSocket.writeDatagram(message, QHostAddress(group_ip), port);
             }
@@ -308,7 +379,7 @@ private:
     {
         QFileInfo file_info(file_path);
         if (!file_info.exists() || !file_info.isFile()) {
-            qDebug() << "无法获取文件大小：";
+//            qDebug() << "无法获取文件大小：";
             return 0;
         }
 
@@ -328,7 +399,7 @@ private:
             if (!client_socket.waitForConnected()) {
                 // 连接失败的处理
                 // 连接失败的处理
-                qDebug() << "连接失败：" << client_socket.errorString();
+//                qDebug() << "连接失败：" << client_socket.errorString();
                 continue;
             }
             QString linux_path = file_path;
@@ -343,14 +414,14 @@ private:
             qint64 file_size = get_file_size(liunx_filename);
             // 将文件大小转换为十六进制
             QString hex_string = "*" + calculate_crc32(liunx_filename);
-            qDebug() << liunx_filename;
+//            qDebug() << liunx_filename;
             // 发送文件名
             QString message = QString("/%1/%2|%3%4\r\n").arg(user_code).arg(file_path).arg(file_size).arg(hex_string);
 
             client_socket.write(message.toUtf8());
             if (!client_socket.waitForBytesWritten())
             {
-               qDebug() << "Failed to send packet.";
+//               qDebug() << "Failed to send packet.";
                continue;
             }
             // 读取文件数据并发送给服务器
@@ -361,11 +432,11 @@ private:
                     }
                     while (!file.atEnd()) {
                         QByteArray data = file.readAll();
-                        qDebug() << data;
+//                        qDebug() << data;
                         client_socket.write(data);
                         if (!client_socket.waitForBytesWritten())
                        {
-                           qDebug() << "Failed to send packet.";
+//                           qDebug() << "Failed to send packet.";
                            continue;
                        }
                     }
@@ -373,20 +444,20 @@ private:
 
                     file.close();
                 } catch (const QString& error) {
-                    qDebug() << "Error: " << error;
+//                    qDebug() << "Error: " << error;
                 }
 
             // 关闭连接
             client_socket.disconnectFromHost();
         }
-        qDebug() << "文件发送完成。";
+//        qDebug() << "文件发送完成。";
     }
 
     QString calculate_crc32(const QString& file_path)
     {
         QFile file(file_path);
         if (!file.open(QIODevice::ReadOnly)) {
-            qDebug() << "无法打开文件：" << file_path;
+//            qDebug() << "无法打开文件：" << file_path;
             return QString();
         }
 
@@ -403,7 +474,7 @@ private:
     void receiveFileThread()
     {
 
-        qDebug() << "Server started. Waiting for client connections...";
+//        qDebug() << "Server started. Waiting for client connections...";
 
         while (true)
         {
@@ -412,7 +483,7 @@ private:
 
             // 接受客户端连接
             QTcpSocket *clientSocket = server.nextPendingConnection();
-            qDebug() << "Client connected:" << clientSocket->peerAddress().toString() << clientSocket->peerPort();
+            //qDebug() << "Client connected:" << clientSocket->peerAddress().toString() << clientSocket->peerPort();
 
             QByteArray receivedData;
             bool endMarkerFound = false;
@@ -441,21 +512,22 @@ private:
 
             if (endMarkerFound)
             {
-                qDebug() << "Received data:" << receivedData;
-
+//                qDebug() << "Received data:" << receivedData;
+//qDebug() << "Received datadddd:" << receivedData;
                 QString filePath = receivedData;
                 filePath = filePath.split('|').at(0);
+                QString file_size_and_crc_32 = filePath.split("|").at(1);
 
                 // 提取文件名
                 QString filename = QDir::homePath() + "/" + QRegularExpression("[^\\\\/:*?\"<>|\\r\\n]+$").match(filePath).captured();
-                qDebug() << filename;
+//                qDebug() << filename;
                 // 接收文件数据并写入文件
                 QFile file(filename);
                 if (file.open(QIODevice::WriteOnly))
                 {
                     while (clientSocket->state() == QAbstractSocket::ConnectedState)
                     {
-                         qDebug() << clientSocket->state();
+//                         qDebug() << clientSocket->state();
                         // 接收数据
                         if (clientSocket->bytesAvailable() > 0)
                         {
@@ -469,12 +541,37 @@ private:
 
 
                     }
+                    bool ok;
+                    quint32 file_size_receive = file_size_and_crc_32.split("*").at(0).toUInt(&ok);
+                    if (!ok) {
+//                        qDebug() << "convert_fail:";
+                        // 转换失败的处理逻辑
+                    }
+                    QString crc_32_receive = file_size_and_crc_32.split("*").at(1);
+                    QString crc_32_file = calculate_crc32(filename);
+                    qint64 file_size = get_file_size(filename);
+//                    qDebug() <<  "1111";
+                    //qDebug() << file_size << "=" << file_size_receive << "=" << crc_32_receive << "=" << crc_32_file;
+                    if(file_size != file_size_receive || crc_32_receive != crc_32_file){
+                        //qDebug() << "File 111:" << filename;
+                        QMessageBox message_box(this);
+                        message_box.setWindowTitle("提示");
+                        message_box.setText(QString("收到文件，已保存到%1,CRC校验失败【%2】").arg(filename, crc_32_file));
+                        message_box.exec();
+                    } else{
+                        //qDebug() << "File 222:" << filename;
+                        QMessageBox message_box(this);
+                        message_box.setWindowTitle("提示");
+                        message_box.setText(QString("收到文件，已保存到%1,CRC校验通过【%2】").arg(filename, crc_32_file));
+                        message_box.exec();
+                    }
 
-                    qDebug() << "File received:" << filename;
+                    //calculate_crc32()
+                    //qDebug() << "File received:" << filename;
                 }
                 else
                 {
-                    qDebug() << "Failed to open file:" << filename;
+                    //qDebug() << "Failed to open file:" << filename;
                 }
 
                 file.close();
